d16041c0471b730f73c8d0bb497ab4c38bd7a540
Ilya Kurganov <iluha.kurganov@yandex.ru>
2024-07-15 17:47:32 +0500

    Добавляю классы в input

--- src/input/ManualDataReader.java
+++ src/input/ManualDataReader.java
@@ -1,11 +1,33 @@
 package input;
 
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class ManualDataReader<T extends Number> implements DataReader<T> {
+public class ManualDataReader implements DataReader<Integer> {
 
     @Override
-    public T[] getData(int length) throws FileNotFoundException {
-        return null;
+    public Integer[] getData(int length) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> data = new ArrayList<>();
+        System.out.println("Введите числа (введите 'end' для завершения):");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("end")) {
+                if (data.isEmpty()) {
+                    System.out.println("Необходимо ввести хотя бы одно число. Пожалуйста, введите числа:");
+                    continue;
+                }
+                break;
+            }
+            try {
+                data.add(Integer.parseInt(input.trim()));
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный ввод: " + input + ". Пожалуйста, введите корректное целое число.");
+            }
+        }
+
+        return data.toArray(new Integer[0]);
     }
 }
